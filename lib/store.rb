b2c54a4e63a9bcd11abd44e34259013e9b73c183
Kenneth Berland <ken@hero.net>
2020-08-08 20:29:28 -0700

    Update store.rb

--- lib/store.rb
+++ lib/store.rb
@@ -1,15 +1,15 @@
 class Store
   attr_accessor :lat, :long, :address, :city, :state, :zip, :area_code, :phone, :name, :geohash
   def initialize(store_data)
-    @lat       = store_data['latitude']
-    @long      = store_data['longitude']
-    @address   = store_data['address']
-    @city      = store_data['city']
-    @state     = store_data['state']
-    @zip       = store_data['zip']
-    @area_code = store_data['area_code']
-    @phone     = store_data['phone']
-    @name      = store_data['name']
-    @geohash   = store_data['geohash'] || Geohash.encode(lat, long, 10)
+    @lat       = store_data[:latitude]
+    @long      = store_data[:longitude]
+    @address   = store_data[:address]
+    @city      = store_data[:city]
+    @state     = store_data[:state]
+    @zip       = store_data[:zip]
+    @area_code = store_data[:area_code]
+    @phone     = store_data[:phone]
+    @name      = store_data[:name]
+    @geohash   = store_data[:geohash] || Geohash.encode(lat, long, 10)
   end
 end
